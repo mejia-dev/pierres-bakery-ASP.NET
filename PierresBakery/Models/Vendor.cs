9399cce516b42528e33a23b09489ccc238e90e02
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-26 11:06:33 -0800

    Pass seventh test for Vendor

--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -6,11 +6,14 @@ namespace PierresBakery.Models
   {
     public int Id { get; }
     public List<Order> Orders { get; }
+    public string Name { get; }
     private static List<Vendor> _instances = new List<Vendor> {};
-    public Vendor()
+    public Vendor(string name)
     {
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order> {};
+      Name = name;
     }
     public static List<Vendor> GetAll()
     {

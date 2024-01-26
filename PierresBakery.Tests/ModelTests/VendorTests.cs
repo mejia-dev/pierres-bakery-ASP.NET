f01b6eb46265b29cf05da7ab4236c0f1356f022e
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-26 08:24:38 -0800

    Write first test for Vendor

---
+++ PierresBakery.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor testVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
+  }
+
+}

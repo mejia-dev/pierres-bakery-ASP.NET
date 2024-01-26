9eb290bed7f8df70b512f180a297aa9d89a08ba6
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-26 11:26:26 -0800

    Pass third test for Order

--- PierresBakery/Models/Order.cs
+++ PierresBakery/Models/Order.cs
@@ -6,11 +6,13 @@ namespace PierresBakery.Models
   {
     private static List<Order> _instances = new List<Order> { };
     public string Title { get; }
+    public string Description { get; }
 
-    public Order(string title)
+    public Order(string title, string description)
     {
       _instances.Add(this);
       Title = title;
+      Description = description;
     }
   }
 }

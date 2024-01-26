13ad5b009e0696368745fcb71761e1c9cf9e2214
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-26 10:27:20 -0800

    Write third test for Vendor

--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Vendor
   {
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> {};
     public Vendor()
     {
-
+      _instances.Add(this);
+      Id = _instances.Count;
     }
   }
 }
